a1a4961b6fbfc70c01edc1eaf9e3f1dce6984239
Mordechaie <mordynys@gmail.com>
2024-11-14 00:30:29 -0500

    added a blog post

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey("auth.user", on_delete=models.CASCADE)
     body = models.TextField()
 
     def __str__(self):
